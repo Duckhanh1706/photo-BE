39113393c1d5545b318034df9fff2725f01013dc
Nguyễn Đức Khánh B22DCCN447- <121552717+Duckhanh1706@users.noreply.github.com>
2026-05-06 08:11:30 +0000

    1

--- routes/PhotoRouter.js
+++ routes/PhotoRouter.js
@@ -1,13 +1,45 @@
 const express = require("express");
-const Photo = require("../db/photoModel");
 const router = express.Router();
+const Photo = require("../db/photoModel");
+const User = require("../db/userModel");
 
-router.post("/", async (request, response) => {
-  
-});
+// photos of user
+router.get("/photosOfUser/:id", async (req, res) => {
+  try {
+    const photos = await Photo.find({ user_id: req.params.id }).lean();
 
-router.get("/", async (request, response) => {
-  
+    const result = await Promise.all(
+      photos.map(async (photo) => {
+        const comments = await Promise.all(
+          (photo.comments || []).map(async (c) => {
+            const user = await User.findById(
+              c.user_id,
+              "_id first_name last_name"
+            ).lean();
+
+            return {
+              _id: c._id,
+              comment: c.comment,
+              date_time: c.date_time,
+              user,
+            };
+          })
+        );
+
+        return {
+          _id: photo._id,
+          user_id: photo.user_id,
+          file_name: photo.file_name,
+          date_time: photo.date_time,
+          comments,
+        };
+      })
+    );
+
+    res.json(result);
+  } catch (err) {
+    res.status(400).send("Error");
+  }
 });
 
 module.exports = router;
--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -1,13 +1,31 @@
 const express = require("express");
-const User = require("../db/userModel");
 const router = express.Router();
+const User = require("../db/userModel");
 
-router.post("/", async (request, response) => {
-  
+// 1. user list
+router.get("/list", async (req, res) => {
+  try {
+    const users = await User.find({}, "_id first_name last_name");
+    res.json(users);
+  } catch (err) {
+    res.status(500).send(err);
+  }
 });
 
-router.get("/", async (request, response) => {
-  
+// 2. user detail
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(
+      req.params.id,
+      "_id first_name last_name location description occupation"
+    );
+
+    if (!user) return res.status(400).send("Invalid user id");
+
+    res.json(user);
+  } catch (err) {
+    res.status(400).send("Invalid id");
+  }
 });
 
 module.exports = router;
