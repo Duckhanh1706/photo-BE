ab4fa3cd72cb843090d501c832da89c6d366bd9f
Nguyễn Đức Khánh B22DCCN447- <121552717+Duckhanh1706@users.noreply.github.com>
2026-05-06 08:24:10 +0000

    1

--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -1,18 +1,46 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../db/userModel");
+const Photo = require("../db/photoModel");
 
-// 1. user list
 router.get("/list", async (req, res) => {
   try {
-    const users = await User.find({}, "_id first_name last_name");
-    res.json(users);
+    const users = await User.find({}, "_id first_name last_name").lean();
+    const photos = await Photo.find({}, "user_id comments").lean();
+
+    const result = users.map((u) => {
+      const uid = u._id.toString();
+
+      // 📷 photo count
+      const photoCount = photos.filter(
+        (p) => p.user_id?.toString() === uid
+      ).length;
+
+      // 💬 comment count
+      let commentCount = 0;
+
+      photos.forEach((p) => {
+        (p.comments || []).forEach((c) => {
+          if (c.user_id?.toString() === uid) {
+            commentCount++;
+          }
+        });
+      });
+
+      return {
+        ...u,
+        photoCount,
+        commentCount,
+      };
+    });
+
+    res.json(result);
   } catch (err) {
-    res.status(500).send(err);
+    console.error(err);
+    res.status(500).send("Server error");
   }
 });
 
-// 2. user detail
 router.get("/:id", async (req, res) => {
   try {
     const user = await User.findById(
